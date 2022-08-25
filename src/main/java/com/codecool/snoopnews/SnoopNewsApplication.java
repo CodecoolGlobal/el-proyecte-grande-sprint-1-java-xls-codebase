8a4eeee2fd00c3ab4e833f51d5e6b10ba18d3f1d
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-08-25 11:54:33 +0200

    task: project initialization, color-scheme

---
+++ src/main/java/com/codecool/snoopnews/SnoopNewsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.snoopnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoopNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnoopNewsApplication.class, args);
+    }
+
+}
