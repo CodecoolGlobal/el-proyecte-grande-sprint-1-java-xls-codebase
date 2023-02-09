484d0de0f5dcd7dac56cbdbcca50920acb451598
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2023-02-09 14:32:34 +0100

    Bugfix: LazyInitializationException

--- modules/oauth2-authorization-server/src/main/java/com/codecool/oauth2_authorization_server/users/model/User.java
+++ modules/oauth2-authorization-server/src/main/java/com/codecool/oauth2_authorization_server/users/model/User.java
@@ -29,7 +29,7 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<UserRole> userRoles = new HashSet<>();
 }
