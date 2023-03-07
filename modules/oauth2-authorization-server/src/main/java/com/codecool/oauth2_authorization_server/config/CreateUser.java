f93558cbfba824b2c8c3d5caf85370dde2ee68ec
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2023-03-07 22:21:32 +0100

    bugfix: CreateUser(init() was not running, password-encoding malfunction)

--- modules/oauth2-authorization-server/src/main/java/com/codecool/oauth2_authorization_server/config/CreateUser.java
+++ modules/oauth2-authorization-server/src/main/java/com/codecool/oauth2_authorization_server/config/CreateUser.java
@@ -1,42 +1,32 @@
 package com.codecool.oauth2_authorization_server.config;
 
-import com.codecool.oauth2_authorization_server.users.model.RoleType;
 import com.codecool.oauth2_authorization_server.users.model.User;
-import com.codecool.oauth2_authorization_server.users.model.UserRole;
-import com.codecool.oauth2_authorization_server.users.service.UserRoleService;
 import com.codecool.oauth2_authorization_server.users.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Set;
+import javax.annotation.PostConstruct;
 
 @Configuration
 @AllArgsConstructor
 public class CreateUser {
 
     private final UserService userService;
-    private final UserRoleService userRoleService;
     PasswordEncoder passwordEncoder;
 
 
+    @PostConstruct
     public void init() {
-        String encodedPassword = passwordEncoder.encode("password");
-
-        UserRole userRole = UserRole.builder()
-                .name(RoleType.USER)
-                .build();
-
-        UserRole savedUserRole = userRoleService.add(userRole);
-
+//        String encodedPassword = passwordEncoder.encode("password");
 
         User user = User.builder()
                 .username("user")
                 .email("email@email.com")
-                .password(encodedPassword)
+//                .password(encodedPassword)
+                .password("password")
                 .firstName("Axel")
                 .lastName("K.")
-                .userRoles(Set.of(savedUserRole))
                 .build();
 
         userService.add(user);
